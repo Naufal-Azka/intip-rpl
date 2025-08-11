26c1cde20f75d6272e8277adfb1d92815f98fd22
Naufal-Azka <askanaufal74@gmail.com>
2025-08-11 08:20:41 +0700

    commit

--- src/app/api/laporan/route.ts
+++ src/app/api/laporan/route.ts
@@ -11,6 +11,7 @@ const prisma = new PrismaClient();
 
 // Inisialisasi Cloudinary dengan env
 cloudinary.config({
+    cloud_url: process.env.CLOUDINARY_URL || 'CLOUDINARY_URL=cloudinary://758862139316742:XRQk7Flp1SZiHzaWv07Fr6oSbWc@dflobvq8c',
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dflobvq8c',
     api_key: process.env.CLOUDINARY_API_KEY || '758862139316742',
     api_secret: process.env.CLOUDINARY_API_SECRET || 'XRQk7Flp1SZiHzaWv07Fr6oSbWcy',
